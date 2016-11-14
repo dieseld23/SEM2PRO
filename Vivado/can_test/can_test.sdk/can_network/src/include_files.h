f19bab26fc283c829c7452815ca8fc549d3bdcc7
catalin852 <catalin852@ymail.com>
2016-11-14 09:56:48 +0100

    Vivado test project

---
+++ Vivado/can_test/can_test.sdk/can_network/src/include_files.h
@@ -0,0 +1,35 @@
+/*
+ * include_files.h
+ *
+ *  Created on: 10 Nov 2016
+ *      Author: Catalin I. Ntemkas
+ */
+
+#ifndef SRC_INCLUDE_FILES_H_
+#define SRC_INCLUDE_FILES_H_
+
+#ifndef XPARAMETERS
+#include "xparameters.h"
+#endif
+
+#ifndef XGPIO
+#include "xgpio.h"
+#endif
+
+#ifndef XSTATUS
+#include "xstatus.h"
+#endif
+
+#ifndef XSCUGIC
+#include "xscugic.h"
+#endif
+
+#ifndef XCANPS
+#include "xcanps.h"
+#endif
+
+#ifndef XIL_PRINTF
+#include "xil_printf.h"
+#endif
+
+#endif /* SRC_INCLUDE_FILES_H_ */
