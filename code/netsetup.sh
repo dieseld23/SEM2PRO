b3608008c50819c3a5ded3863e15559710d864da
Thomas Christensen <thomas.s.christensen101@gmail.com>
2016-11-30 16:10:45 +0100

    added correct netsetup.sh ... derp...

--- code/netsetup.sh
+++ code/netsetup.sh
@@ -3,6 +3,11 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+#Find your device name using:
+#	ip link show
+
+#choose the wifi device
+
 systemctl stop connman.service
 ip link set wlp2s0b1 down
 iw wlp2s0b1 set type ibss
